17bbda0334458c778055bd021ee0a565754cb374
Sangar <florian@nuecke.de>
2025-12-02 17:21:15 +0000

    day to, still in go...

---
+++ 02/go.mod
@@ -0,0 +1,3 @@
+module aoc25/day2
+
+go 1.25.4
---
+++ 02/main.go
@@ -0,0 +1,35 @@
+package main
+
+func Run(input []Action) int {
+	count := 0
+	acc := 50
+	for _, action := range input {
+		delta := int(action.direction) * action.distance
+		if delta >= 100 {
+			count += delta / 100
+			delta = delta % 100
+		}
+		if delta <= -100 {
+			count += (-delta) / 100
+			delta = -((-delta) % 100)
+		}
+
+		if delta == 0 {
+			continue
+		}
+
+		acc += delta
+		if acc == 0 {
+			count += 1
+		} else if acc < 0 {
+			if acc != delta {
+				count += 1
+			}
+			acc += 100
+		} else if acc > 99 {
+			acc -= 100
+			count += 1
+		}
+	}
+	return count
+}
